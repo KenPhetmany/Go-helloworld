8b5dfd62dcbfae6102e7a1c036d01f172bd9d1ee
Ken Phetmany <ken.phetmany@optus.com.au>
2023-03-24 10:25:25 +1100

    Added static files for css

--- hello.go
+++ hello.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	// Creates router object
@@ -11,6 +14,19 @@ func main() {
 		c.String(200, "Hello, World")
 	})
 
+	// Create a group of routes behind /api
+	api := r.Group("/api")
+
+	// Return a JSON response
+	api.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// Serve Static files
+	r.Use(static.Serve("/", static.LocalFile("./views", true)))
+
 	// Tell the webserver to listen on port 3000
-	r.Run(":3000")
+	r.Run()
 }
